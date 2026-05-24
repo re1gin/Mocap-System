f679c8b58385702d9eaa8d8ffb3d61a7c9d26a71
Rei_Gin <marhamdiihwal70@gmail.com>
2026-05-25 06:15:04 +0800

    Add head and spine offset adjustments with sliders; enhance joint calculations

--- main.py
+++ main.py
@@ -18,9 +18,12 @@ L_LOWER_OFFSET = 0
 R_UPPER_OFFSET = 90
 R_LOWER_OFFSET = 0
 
-# ==============================================================================
+HEAD_OFFSET = 0
+SPINE_OFFSET = 0
+
+# =====================
 # THREAD MEDIA PIPE
-# ==============================================================================
+# =====================
 class MocapThread(threading.Thread):
     def __init__(self):
         super().__init__(daemon=True)
@@ -40,6 +43,11 @@ class MocapThread(threading.Thread):
         self.r_upper = 0.0
         self.r_lower = 0.0
         
+        self.head_roll = 0.0
+        self.spine_roll = 0.0
+        self.r_hand_flipped = False
+        self.l_hand_flipped = False
+        
         self.current_frame = None
 
     def run(self):
@@ -55,34 +63,52 @@ class MocapThread(threading.Thread):
 
             if results.pose_landmarks:
                 lm = results.pose_landmarks.landmark
-                if all(lm[i].visibility > 0.7 for i in [11,13,15,12,14,16]):
+                if all(lm[i].visibility > 0.7 for i in [11,13,15,12,14,16,7,8,]): #17,18,21,22,23,24,
                     self.pose_detected = True
                     mp_drawing.draw_landmarks(frame, results.pose_landmarks, self.mp_pose.POSE_CONNECTIONS)
-
-                    # Perhitungan Sudut Sederhana
+                    
+                    l_upper_vec = (lm[13].x - lm[11].x, lm[13].y - lm[11].y)
+                    l_lower_vec = (lm[15].x - lm[13].x, lm[15].y - lm[13].y)
+                    
+                    r_upper_vec = (lm[14].x - lm[12].x, lm[14].y - lm[12].y)
+                    r_lower_vec = (lm[16].x - lm[14].x, lm[16].y - lm[14].y)
                     
                     # Lengan Kiri
-                    l_upper_angle = math.degrees(math.atan2(lm[13].y - lm[11].y, lm[13].x - lm[11].x))
+                    """l_upper_angle = math.degrees(math.atan2(lm[13].y - lm[11].y, lm[13].x - lm[11].x))
                     self.l_upper = normalize_angle(-l_upper_angle - 90)
+                    self.l_lower = math.degrees(math.atan2(lm[15].y - lm[13].y, lm[15].x - lm[13].x)) - 90"""
                     
-                    self.l_lower = math.degrees(math.atan2(lm[15].y - lm[13].y, lm[15].x - lm[13].x)) - 90
-
+                    l_upper_angle = math.degrees(math.atan2(l_upper_vec[1], l_upper_vec[0]))
+                    self.l_upper = normalize_angle(-l_upper_angle - 90)
+                    elbow_angle = angle_between(l_upper_vec, l_lower_vec)
+                    self.l_lower = -(elbow_angle)
+                    
                     # Lengan Kanan
-
-                    r_upper_raw = math.degrees(math.atan2(lm[14].y - lm[12].y, lm[14].x - lm[12].x))
+                    """r_upper_raw = math.degrees(math.atan2(lm[14].y - lm[12].y, lm[14].x - lm[12].x))
                     self.r_upper = normalize_angle(-r_upper_raw - 90)
-                    self.r_lower = math.degrees(math.atan2(lm[16].y - lm[14].y, lm[16].x - lm[14].x)) - 90
+                    self.r_lower = math.degrees(math.atan2(lm[16].y - lm[14].y, lm[16].x - lm[14].x)) - 90"""
+
+                    r_upper_angle = math.degrees(math.atan2(r_upper_vec[1], r_upper_vec[0]))
+                    self.r_upper = normalize_angle(-r_upper_angle - 90)
+                    elbow_angle = angle_between(r_upper_vec, r_lower_vec)
+                    self.r_lower = -(elbow_angle)
+
+                    # Tangan Terbalik
+                    self.r_hand_flipped = lm[22].x > lm[18].x
+                    self.l_hand_flipped = lm[21].x < lm[17].x
                     
-                    print(self.r_upper)
+                    # Kepala
+                    self.head_roll = normalize_angle(math.degrees(math.atan2(lm[7].y - lm[8].y, lm[7].x - lm[8].x)))
                     
-                    # Simpan data untuk debug
-                    self.debug_data = {
-                        'r_shoulder': (lm[12].x, lm[12].y),
-                        'r_elbow':    (lm[14].x, lm[14].y),
-                        'r_wrist':    (lm[16].x, lm[16].y),
-                        'r_upper_raw': r_upper_raw,
-                        'r_upper_final': self.r_upper
-                    }
+                    # Spine
+                    center_shoulder_x = (lm[11].x + lm[12].x) / 2
+                    center_shoulder_y = (lm[11].y + lm[12].y) / 2
+                    center_hip_x = (lm[23].x + lm[24].x) / 2
+                    center_hip_y = (lm[23].y + lm[24].y) / 2
+                    self.spine_roll = normalize_angle(
+                        math.degrees(math.atan2(
+                            center_hip_y - center_shoulder_y, center_hip_x - center_shoulder_x)) 
+                        - 90)
 
                 else:
                     self.pose_detected = False
@@ -95,9 +121,9 @@ class MocapThread(threading.Thread):
         self.cap.release()
 
 
-# ==============================================================================
+# ================
 # URSINA SETUP
-# ==============================================================================
+# ================
 app = Ursina()
 window.title = "Simple Motion Capture"
 window.borderless = False
@@ -120,15 +146,19 @@ cam_texture = Texture(panda_tex)
 Entity(parent=camera.ui, model='quad', texture=cam_texture, position=(-0.65, 0.3), scale=(0.55, 0.4))
 
 # Joints
-l_upper = l_lower = r_upper = r_lower = None
+l_upper = l_lower = l_hand = r_upper = r_lower = r_hand = c_head = c_spine = None
 
 def init_joints():
-    global l_upper, l_lower, r_upper, r_lower
+    global l_upper, l_lower, r_upper, r_lower, c_head, c_spine
     if not karakter: return
     l_upper = karakter.controlJoint(None, "modelRoot", "J_Bip_L_UpperArm")
     l_lower = karakter.controlJoint(None, "modelRoot", "J_Bip_L_LowerArm")
+    l_hand = karakter.controlJoint(None, "modelRoot", "J_Bip_L_Hand")
     r_upper = karakter.controlJoint(None, "modelRoot", "J_Bip_R_UpperArm")
     r_lower = karakter.controlJoint(None, "modelRoot", "J_Bip_R_LowerArm")
+    r_hand = karakter.controlJoint(None, "modelRoot", "J_Bip_R_Hand")
+    c_head = karakter.controlJoint(None, "modelRoot", "J_Bip_Head")
+    c_spine = karakter.controlJoint(None, "modelRoot", "J_Bip_Spine")
     print("[OK] Joints terhubung")
 
 invoke(init_joints, delay=1.0)
@@ -147,6 +177,8 @@ EditorCamera()
 # Buat text instruksi di layar
 Text(text="Gunakan Slider untuk Tuning Offset secara Real-Time", position=(-0.2, 0.45), scale=1.2)
 
+slider_head    = Slider(min=-180, max=180, default=HEAD_OFFSET, text='Head Offset', position=(0.2, -0.1), dynamic=True)
+slider_spine   = Slider(min=-180, max=180, default=SPINE_OFFSET, text='Spine Offset', position=(0.2, -0.15), dynamic=True)
 slider_l_upper = Slider(min=-180, max=180, default=L_UPPER_OFFSET, text='L Upper Offset', position=(0.2, -0.2), dynamic=True)
 slider_l_lower = Slider(min=-180, max=180, default=L_LOWER_OFFSET, text='L Lower Offset', position=(0.2, -0.25), dynamic=True)
 slider_r_upper = Slider(min=-180, max=180, default=R_UPPER_OFFSET, text='R Upper Offset', position=(0.2, -0.3), dynamic=True)
@@ -158,6 +190,20 @@ tracking_was_active = False
 
 frame_count = 0
 tracking_was_active = False
+
+def angle_between(v1, v2):
+    dot = v1[0]*v2[0] + v1[1]*v2[1]
+
+    mag1 = math.sqrt(v1[0]**2 + v1[1]**2)
+    mag2 = math.sqrt(v2[0]**2 + v2[1]**2)
+
+    if mag1 == 0 or mag2 == 0:
+        return 0
+
+    cos_angle = dot / (mag1 * mag2)
+    cos_angle = max(-1, min(1, cos_angle))
+
+    return math.degrees(math.acos(cos_angle))
 
 def normalize_angle(angle):
     while angle > 180:
@@ -190,15 +236,26 @@ def update():
             print("Tracking Regained -> Mengambil alih kendali joint kembali...")
             init_joints()
             tracking_was_active = True
+            
+        if c_head:
+            target_head = ai_mocap.head_roll + slider_head.value
+            c_head.setR(lerp(c_head.getR(), target_head, current_smooth))
 
-        # Ambil nilai target secara dinamis dari posisi SLIDER saat ini
+        if c_spine:
+            target_spine = ai_mocap.spine_roll + slider_spine.value
+            c_spine.setR(lerp(c_spine.getR(), target_spine, current_smooth))
+            
         if l_upper:
             target = ai_mocap.l_upper + slider_l_upper.value
             l_upper.setP(lerp(l_upper.getP(), target, current_smooth))
 
         if l_lower:
             target = -ai_mocap.l_lower + slider_l_lower.value
-            l_lower.setR(lerp(l_lower.getR(), target, current_smooth))
+            l_lower.setP(lerp(l_lower.getP(), target, current_smooth))
+            
+        if l_hand:
+            wrist_target = clamp(ai_mocap.l_upper * 0.5, -45, 45)
+            l_hand.setR(lerp(l_hand.getR(), wrist_target, current_smooth))
 
         if r_upper:
             target = -ai_mocap.r_upper + slider_r_upper.value
@@ -206,8 +263,11 @@ def update():
 
         if r_lower:
             target = -ai_mocap.r_lower + slider_r_lower.value
-            r_lower.setR(lerp(r_lower.getR(), target, current_smooth))
-
+            r_lower.setP(lerp(r_lower.getP(), target, current_smooth))
+            
+        if r_hand:
+            wrist_target = clamp(ai_mocap.r_lower * 0.5, -45, 45)
+            r_hand.setH(lerp(r_hand.getH(), wrist_target, current_smooth))
     else:
         if tracking_was_active:
             print("Tracking Lost -> Melepaskan kendali joint...")
